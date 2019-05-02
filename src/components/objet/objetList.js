dc2f06d046d495df9e27f447d0b5ee9a11d9b4df
fviaud <frederic.viaud@hotmail.fr>
2019-05-02 13:17:19 +0200

    commit

--- src/components/objet/objetList.js
+++ src/components/objet/objetList.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import ObjetElement from './objetElement';
-import { TryDelObjet,fetchObjet} from "../../store/actions";
-import {Loading} from '../../components';
+import ObjetElement from "./objetElement";
+import { TryDelObjet, fetchObjet } from "../../store/actions";
+import { Loading } from "../../components";
 
 class ObjetList extends Component {
   constructor(props) {
@@ -11,29 +11,37 @@ class ObjetList extends Component {
     props.fetchObjet();
   }
   render() {
-    const { objets,Isloading,TryDelObjet,IsError } = this.props;
-   
+    const { objets, Isloading, TryDelObjet, IsError } = this.props;
     return (
-        <>
-        {Isloading ? IsError ? <div>{IsError.message}</div> :<Loading/>:
-        <ul className="list-group">
-        { objets && objets.map( (o, i) => (
-          <ObjetElement 
-            key={ i} 
-            objet={ o } 
-            deleteObjet={ () => TryDelObjet(i) }  
-          />
-        )) }
-        </ul>
-        }
-        dev
-        </>
-    )
-    }
+      <>
+        {Isloading ? (
+          IsError ? (
+            <div>{IsError.message}</div>
+          ) : (
+            <Loading />
+          )
+        ) : (
+          <ul className="list-group">
+            {objets &&
+              objets.map((o, i) => (
+                <ObjetElement
+                  key={i}
+                  objet={o}
+                  deleteObjet={() => TryDelObjet(i)}
+                />
+              ))}
+          </ul>
+        )}
+      </>
+    );
+  }
 }
-export default connect(state => {
-  let objets = state.objets.data
-  let Isloading= state.objets.loading
-  let IsError= state.objets.error
-  return {objets,Isloading,IsError};
-}, {TryDelObjet,fetchObjet})(ObjetList);
+export default connect(
+  state => {
+    let objets = state.objets.data;
+    let Isloading = state.objets.loading;
+    let IsError = state.objets.error;
+    return { objets, Isloading, IsError };
+  },
+  { TryDelObjet, fetchObjet }
+)(ObjetList);
